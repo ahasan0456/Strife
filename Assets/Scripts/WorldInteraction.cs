9b38537ed9f261835b8680b726b114399831696d
Amir Hasan <ahasan0456@bths.edu>
2018-03-20 13:17:21 -0400

    In progress of backwards walking.

--- Assets/Scripts/WorldInteraction.cs
+++ Assets/Scripts/WorldInteraction.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class WorldInteraction : MonoBehaviour {
 	public float speed;
-
+	static Animator anim;
+	public float walkAnimationSpeed;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+		anim.speed = walkAnimationSpeed;
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
+	// FixedUpdate is called several times per frame
+	void FixedUpdate ()
+	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical")*Time.deltaTime*speed;
 
 		transform.Rotate (0, moveHorizontal, 0);
 		transform.Translate (0, 0, moveVertical);
+
+		if (moveVertical > 0)
+		{
+			anim.SetBool ("isWalking", true);
+		}
+		else if (moveVertical < 0)
+		{
+			anim.SetBool ("isWalkingBack", true);
+		}
+		else
+		{
+			anim.SetBool ("isWalking",false);
+		}
 	}
 }
