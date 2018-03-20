0eb4adf232ac62a5a4ae58dce22a569448b6c343
Amir Hasan <ahasan0456@bths.edu>
2018-03-19 22:23:01 -0400

    Initial project setup.

---
+++ Assets/WorldInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInteraction : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		transform.Rotate (0, moveHorizontal, 0);
+		transform.Translate (0, 0, moveVertical * speed);
+	}
+}
