11077c33d36f3af542c80938abcdeefebf99ae74
Amir Hasan <ahasan0456@bths.edu>
2018-03-20 12:47:18 -0400

    Added mesh collider.

--- Assets/Scripts/WorldInteraction.cs
+++ Assets/Scripts/WorldInteraction.cs
@@ -13,9 +13,9 @@ public class WorldInteraction : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveVertical = Input.GetAxis ("Vertical")*Time.deltaTime*speed;
 
 		transform.Rotate (0, moveHorizontal, 0);
-		transform.Translate (0, 0, moveVertical * speed);
+		transform.Translate (0, 0, moveVertical);
 	}
 }
